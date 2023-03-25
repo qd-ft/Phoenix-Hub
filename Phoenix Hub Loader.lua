066c4d8d8454bbe551482fc8160282d8b3959c13
Wolfyz16 <122695031+Wolfyz16@users.noreply.github.com>
2023-03-25 16:12:41 +0000

    Update Phoenix Hub Loader.lua

--- Phoenix Hub Loader.lua
+++ Phoenix Hub Loader.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 8540346411 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Rebirth%20Champions%20X.lua"))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Universal.lua"))()
 end
