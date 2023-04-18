ce3b3c6d84ab707173e9b86dc9d761dbd31e8db2
Wolfyz16 <122695031+Wolfyz16@users.noreply.github.com>
2023-04-18 21:47:20 +0100

    Update Phoenix Hub Loader.lua

--- Phoenix Hub Loader.lua
+++ Phoenix Hub Loader.lua
@@ -1,9 +1,19 @@
+-- Rebirth Champions X
 if game.PlaceId == 8540346411 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Rebirth%20Champions%20X.lua"))()
+-- Tapping Masters
 elseif game.PlaceId == 12422074717 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Tapping%20Masters.lua"))()
+-- Prison Life
 elseif game.PlaceId == 155615604 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Prison%20Life.lua"))()
+-- Giant Survival
+elseif game.PlaceId == 4003872968 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Giant%20Survival.lua"))()
+-- Pet Simulator X
+elseif game.PlaceId == 6284583030 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Pet%20Simulator%20X.lua"))()
+-- Universal
 else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz16/Phoenix-Hub/main/Universal.lua"))()
 end
