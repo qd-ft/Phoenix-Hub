e28d0853eab4a93b655f9846fdb906fcf09396d4
qd-ft <122695031+qd-ft@users.noreply.github.com>
2023-07-05 00:07:27 +0100

    Update Tapping Simulator.lua

--- Tapping Simulator.lua
+++ Tapping Simulator.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Right Now We Hvae Not Finished This Game. Sorry For The Inconvenience. From Phoenix Hub Owner qdft")
+game.Players.LocalPlayer:Kick("Right Now We Have Not Finished This Game. Sorry For The Inconvenience. From Phoenix Hub Owner qdft")
